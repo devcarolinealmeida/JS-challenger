9ef0323304edb92a3b56ae7e2958727f3fa5c62a
devcarolinealmeida <carolineitalia85@gmail.com>
2022-09-19 16:05:07 +0200

    Check if number is Even

---
+++ JavaScript_Basics/CheckIfNumberIsEven.js
@@ -0,0 +1,11 @@
+// Write a function that takes a number as argument
+// If the number is even, return true
+// Otherwise, return false
+function myFunction(a) {
+console.log(a%2 == 0 ? true : false)
+}
+
+myFunction(10) // Expected true
+myFunction(-4) // Expected true
+myFunction(5) // Expected false
+myFunction(-111) // Expected false
