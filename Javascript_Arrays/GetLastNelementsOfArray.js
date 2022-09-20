f3084861a7833f0b698eddeea40c3497f90babba
devcarolinealmeida <carolineitalia85@gmail.com>
2022-09-20 15:58:28 +0200

    Get Last N elements Of Array

---
+++ Javascript_Arrays/GetLastNelementsOfArray.js
@@ -0,0 +1,11 @@
+// Write a function that takes an array (a) as argument
+// Extract the last 3 elements of 'a'
+// Return the resulting array
+function myFunction(a) {
+    console.log(a.slice(- 3))
+return 
+}
+
+myFunction([1,2,3,4]) // Expected [2,3,4]
+myFunction([5,4,3,2,1,0]) // Expected [2,1,0]
+myFunction([99,1,1]) // Expected [99,1,1]
