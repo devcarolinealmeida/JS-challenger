a2b6bc59a6d5df252211af816d8a6aa4f15cbbbc
devcarolinealmeida <carolineitalia85@gmail.com>
2022-09-21 10:00:31 +0200

    Remove Specific Array Element

---
+++ Javascript_Arrays/RemoveSpecificArrayElement.js
@@ -0,0 +1,13 @@
+// Write a function that takes an array (a) and a value (b) as argument
+// The function should remove all elements equal to 'b' from the array
+// Return the filtered array
+function myFunction(a, b){
+    console.log(a.filter(el => el !== b))  
+}
+
+
+myFunction([1,2,3], 2) //Expected [1, 3]
+myFunction([1,2,'2'], '2') // Expected [1, 2]
+myFunction([false,'2',1], false) // Expected ['2', 1]
+myFunction([1,2,'2',1], 1) // Expected [2, '2']
+
