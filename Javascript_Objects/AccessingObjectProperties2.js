73890a038da8e1f65ed0a3609f0bf96419a47e3a
devcarolinealmeida <carolineitalia85@gmail.com>
2022-09-22 09:52:13 +0200

    Accessing Object Properties 2

---
+++ Javascript_Objects/AccessingObjectProperties2.js
@@ -0,0 +1,10 @@
+// Write a function that takes an object with two properties as argument
+// It should return the value of the property with key 'prop-2'
+// Tipp: you might want to use the square brackets property accessor
+function myFunction (obj) {
+    console.log(obj['prop-2'])
+return 
+}
+
+myFunction({  one: 1,  'prop-2': 2}) // Expected 2
+myFunction({  'prop-2': 'two',  prop: 'test'}) // Expected 'two'
