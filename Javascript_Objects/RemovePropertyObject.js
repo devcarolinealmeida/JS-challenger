8a24b6734b6be97d9f009bf14de1cb97e3fcb776
devcarolinealmeida <carolineitalia85@gmail.com>
2022-09-24 10:54:51 +0200

    Remove Property Object

---
+++ Javascript_Objects/RemovePropertyObject.js
@@ -0,0 +1,13 @@
+// Write a function that takes an object as argument
+// It should return an object with all original object properties
+// except for the property with key 'b'
+function myFunction (obj){
+    delete obj.b;
+    console.log(obj)
+ 
+return 
+}
+
+myFunction({ a: 1, b: 7, c: 3 }) // Expected { a: 1, c: 3 }
+myFunction({ b: 0, a: 7, d: 8 }) // Expected { a: 7, d: 8 }
+myFunction({ e: 6, f: 4, b: 5, a: 3 }) // Expected { e: 6, f: 4, a: 3 }
